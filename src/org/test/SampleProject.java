2c54365a5bcc52b830a8799c4703079f52e98bb9
Surya1987-P <microsuryapugal87@gmail.com>
2021-09-21 20:59:46 +0530

    Recent update

--- src/org/test/SampleProject.java
+++ src/org/test/SampleProject.java
@@ -1,5 +1,10 @@
 package org.test;
 
 public class SampleProject {
+	public static void main(String[] args) {
+		System.out.println("GITHUB");
+		System.out.println("BITBUCKET");
+		System.out.println("SCM");
+	}
 
 }
